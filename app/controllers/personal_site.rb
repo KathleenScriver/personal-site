58b0abe26c47cf25d8f7c6d12ed8d29e7122ac12
Kathleen Yruegas <kathleenecn@gmail.com>
2018-08-06 20:22:34 -0600

    Adds more content

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -2,6 +2,36 @@ require 'rack'
 
 class PersonalSite
   def self.call(env)
-    ['200', {'Content-Type' => 'text/html'}, ['Welcome!']]
+    case env["PATH_INFO"]
+    when '/' then index
+    when '/about' then about
+    when '/main.css' then css
+    else
+      error
+    end
+  end
+
+  def self.css
+    render_static('main.css')
+  end
+
+  def self.index
+    ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/index.html')]]
+  end
+
+  def self.about
+    render_view('about.html')
+  end
+
+  def self.error
+    ['404', {'Content-Type' => 'text/html'}, [File.read('./app/views/error.html')]]
+  end
+
+  def self.render_view(page, code = '200')
+    [code, {'Content-Type' => 'text/html'}, [File.read("./app/views/#{page}")]]
+  end
+
+  def self.render_static(asset)
+    ['200', {'Content-Type' => 'text/html'}, [File.read("./public/#{asset}")]]
   end
 end
